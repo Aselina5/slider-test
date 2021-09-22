a858cf7436857f624f2153058ead3fd05b51d92e
aselina5 <aselina90@mail.ru>
2021-09-22 15:01:24 +0600

    inintial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 22,
+        slidesToShow: 1
+    });
+});
